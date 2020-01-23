267c3ddcf41f1f37236226188e80df1e8e57aaef
MUHAMMAD ADIL RAZA <ary18sw18@gmail.com>
2020-01-23 17:53:32 +0500

    Add files via upload

---
+++ src/main/java/audit/com/free_audit/DemoApplication.java
@@ -0,0 +1,16 @@
+package audit.com.free_audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("hello To MAVEN USing spring boot");
+	
+	
+	}
+
+}
